543f448f3c8c26b10532fe7e41cc61d4c032aa25
Stanislau Pandzin <willstesi@gmail.com>
2023-10-19 19:55:27 +0300

    work with layout

--- lib/src/features/welcom_screen/screens/welcom_screen.dart
+++ lib/src/features/welcom_screen/screens/welcom_screen.dart
@@ -1,13 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shelfx/src/features/stores/screens/stores_screen.dart';
 
 class WelcomScreen extends StatelessWidget {
   const WelcomScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Добро пожаловать в Shelf Cloud'),
+    const String welcomingSvg = 'assets/images/welcoming.svg';
+
+    SvgPicture image = SvgPicture.asset(welcomingSvg, fit: BoxFit.cover);
+
+    return Scaffold(
+      backgroundColor: const Color(0xff343B6C),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Center(child: image),
+            const SizedBox(
+              height: 40,
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Добро пожаловать',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Пройдите аутентификацию Shelf Cloud и пользуйтесь всеми функциями приложения.',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Вход'),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const StoresScreen()),
+                      );
+                    },
+                    child: const Text('Регистрация'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,37 +1,19 @@
 import 'package:flutter/material.dart';
 
+const primaryColor = Color(0xff343B6C);
+
 class TAppTheme {
   static ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primarySwatch: const MaterialColor(0xff343B6C, {
-      50: Color(0xffebebf0),
-      100: Color(0xffd6d8e2),
-      200: Color(0xffc2c4d3),
-      300: Color(0xffaeb1c4),
-      400: Color(0xff9a9db6),
-      500: Color(0xff8589a7),
-      600: Color(0xff717698),
-      700: Color(0xff5d6289),
-      800: Color(0xff484f7b),
-      900: Color(0xff343B6C),
-    }),
+    useMaterial3: true,
+    primaryColor: primaryColor,
+    colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
     fontFamily: 'Jost',
   );
 
   static ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: const MaterialColor(0xff343B6C, {
-      50: Color(0xffebebf0),
-      100: Color(0xffd6d8e2),
-      200: Color(0xffc2c4d3),
-      300: Color(0xffaeb1c4),
-      400: Color(0xff9a9db6),
-      500: Color(0xff8589a7),
-      600: Color(0xff717698),
-      700: Color(0xff5d6289),
-      800: Color(0xff484f7b),
-      900: Color(0xff343B6C),
-    }),
+    useMaterial3: true,
+    primaryColor: primaryColor,
+    colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
     fontFamily: 'Jost',
   );
 }
